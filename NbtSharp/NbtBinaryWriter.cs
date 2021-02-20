eeda1018573f993a189540407398f570518ab486
Oliver Booth <me@olivr.me>
2021-02-20 13:25:25 +0000

    Code cleanup for NbtBinaryWriter

--- NbtSharp/NbtBinaryWriter.cs
+++ NbtSharp/NbtBinaryWriter.cs
@@ -3,207 +3,258 @@ using System.IO;
 using System.Text;
 using JetBrains.Annotations;
 
-namespace NbtSharp {
-    /// <summary> BinaryWriter wrapper that writes NBT primitives to a stream,
-    /// while taking care of endianness and string encoding, and counting bytes written. </summary>
-    internal sealed unsafe class NbtBinaryWriter {
-        // Write at most 4 MiB at a time.
-        public const int MaxWriteChunk = 4*1024*1024;
+namespace NbtSharp
+{
+    /// <summary>
+    ///     Represents a <see cref="BinaryWriter" /> wrapper that takes care of reading primitives from an NBT stream, while
+    ///     taking care of endianness, string encoding, and counting bytes written.
+    /// </summary>
+    public class NbtBinaryWriter : BinaryWriter
+    {
+        /// <summary>
+        ///     The maximum writing chunk size. This value is constant. 
+        /// </summary>
+        /// <value>4194304, equivalent to 4 MiB.</value>
+        public const int MaxWriteChunk = 4 * 1024 * 1024; // Write at most 4 MiB at a time.
+        
+        private const int BufferSize = 256;
+        private const int MaxBufferedStringLength = BufferSize / 4; // UTF-8 chars use at most 4 bytes each
+        private static readonly UTF8Encoding Encoding = new(false, true);
+        private readonly Encoder _encoder = Encoding.GetEncoder();
+        private readonly byte[] _buffer = new byte[BufferSize];
+        private readonly Stream _stream;
+        private readonly bool _swapNeeded;
+        
+        /// <summary>
+        ///     Initializes a new instance of the <see cref="NbtBinaryWriter" /> class.
+        /// </summary>
+        /// <param name="output">The output stream.</param>
+        /// <param name="bigEndian">A value indicating whether output is in big endian order.</param>
+        /// <exception cref="ArgumentException">The stream does not support writing or is already closed.</exception>
+        /// <exception cref="ArgumentNullException"><paramref name="output" /> is <see langword="null" />.</exception>
+        public NbtBinaryWriter(Stream output, bool bigEndian)
+            : base(output)
+        {
+            _stream = output;
+            _swapNeeded = (BitConverter.IsLittleEndian == bigEndian);
+        }
 
-        // Encoding can be shared among all instances of NbtBinaryWriter, because it is stateless.
-        static readonly UTF8Encoding Encoding = new UTF8Encoding(false, true);
-
-        // Each instance has to have its own encoder, because it does maintain state.
-        readonly Encoder encoder = Encoding.GetEncoder();
-
-        public Stream BaseStream {
-            get {
-                stream.Flush();
-                return stream;
+        /// <summary>
+        ///     Gets the underlying stream of the <see cref="NbtBinaryWriter" />.
+        /// </summary>
+        /// <value>The underlying stream associated with the <see cref="NbtBinaryWriter" />.</value>
+        /// <remarks>When accessing this value, the underlying stream is flushed.</remarks>
+        public override Stream BaseStream
+        {
+            get
+            {
+                _stream.Flush();
+                return _stream;
             }
         }
 
-        readonly Stream stream;
-
-        // Buffer used for temporary conversion
-        const int BufferSize = 256;
-
-        // UTF8 characters use at most 4 bytes each.
-        const int MaxBufferedStringLength = BufferSize/4;
-
-        // Each NbtBinaryWriter needs to have its own instance of the buffer.
-        readonly byte[] buffer = new byte[BufferSize];
-
-        // Swap is only needed if endianness of the runtime differs from desired NBT stream
-        readonly bool swapNeeded;
-
-
-        public NbtBinaryWriter([NotNull] Stream input, bool bigEndian) {
-            if (input == null) throw new ArgumentNullException("input");
-            if (!input.CanWrite) throw new ArgumentException("Given stream must be writable", "input");
-            stream = input;
-            swapNeeded = (BitConverter.IsLittleEndian == bigEndian);
+        /// <summary>
+        ///     Writes an <see cref="NbtTagType" /> value as represented by an unsigned byte to the current stream and advances
+        ///     the stream position by one byte. 
+        /// </summary>
+        /// <param name="value">The <see cref="NbtTagType" /> to write.</param>
+        /// <exception cref="IOException">An I/O error occurs.</exception>
+        /// <exception cref="ObjectDisposedException">The stream is closed.</exception>
+        public void Write(NbtTagType value)
+        {
+            base.Write((byte)value);
         }
 
-
-        public void Write(byte value) {
-            stream.WriteByte(value);
-        }
-
-
-        public void Write(NbtTagType value) {
-            stream.WriteByte((byte)value);
-        }
-
-
-        public void Write(short value) {
-            unchecked {
-                if (swapNeeded) {
-                    buffer[0] = (byte)(value >> 8);
-                    buffer[1] = (byte)value;
-                } else {
-                    buffer[0] = (byte)value;
-                    buffer[1] = (byte)(value >> 8);
+        /// <inheritdoc />
+        public override void Write(short value)
+        {
+            unchecked
+            {
+                if (_swapNeeded)
+                {
+                    _buffer[0] = (byte)(value >> 8);
+                    _buffer[1] = (byte)value;
+                }
+                else
+                {
+                    _buffer[0] = (byte)value;
+                    _buffer[1] = (byte)(value >> 8);
                 }
             }
-            stream.Write(buffer, 0, 2);
+            _stream.Write(_buffer, 0, 2);
         }
 
 
-        public void Write(int value) {
-            unchecked {
-                if (swapNeeded) {
-                    buffer[0] = (byte)(value >> 24);
-                    buffer[1] = (byte)(value >> 16);
-                    buffer[2] = (byte)(value >> 8);
-                    buffer[3] = (byte)value;
-                } else {
-                    buffer[0] = (byte)value;
-                    buffer[1] = (byte)(value >> 8);
-                    buffer[2] = (byte)(value >> 16);
-                    buffer[3] = (byte)(value >> 24);
+        /// <inheritdoc />
+        public override void Write(int value)
+        {
+            unchecked
+            {
+                if (_swapNeeded)
+                {
+                    _buffer[0] = (byte)(value >> 24);
+                    _buffer[1] = (byte)(value >> 16);
+                    _buffer[2] = (byte)(value >> 8);
+                    _buffer[3] = (byte)value;
+                }
+                else
+                {
+                    _buffer[0] = (byte)value;
+                    _buffer[1] = (byte)(value >> 8);
+                    _buffer[2] = (byte)(value >> 16);
+                    _buffer[3] = (byte)(value >> 24);
                 }
             }
-            stream.Write(buffer, 0, 4);
+            _stream.Write(_buffer, 0, 4);
         }
 
 
-        public void Write(long value) {
-            unchecked {
-                if (swapNeeded) {
-                    buffer[0] = (byte)(value >> 56);
-                    buffer[1] = (byte)(value >> 48);
-                    buffer[2] = (byte)(value >> 40);
-                    buffer[3] = (byte)(value >> 32);
-                    buffer[4] = (byte)(value >> 24);
-                    buffer[5] = (byte)(value >> 16);
-                    buffer[6] = (byte)(value >> 8);
-                    buffer[7] = (byte)value;
-                } else {
-                    buffer[0] = (byte)value;
-                    buffer[1] = (byte)(value >> 8);
-                    buffer[2] = (byte)(value >> 16);
-                    buffer[3] = (byte)(value >> 24);
-                    buffer[4] = (byte)(value >> 32);
-                    buffer[5] = (byte)(value >> 40);
-                    buffer[6] = (byte)(value >> 48);
-                    buffer[7] = (byte)(value >> 56);
+        /// <inheritdoc />
+        public override void Write(long value)
+        {
+            unchecked
+            {
+                if (_swapNeeded)
+                {
+                    _buffer[0] = (byte)(value >> 56);
+                    _buffer[1] = (byte)(value >> 48);
+                    _buffer[2] = (byte)(value >> 40);
+                    _buffer[3] = (byte)(value >> 32);
+                    _buffer[4] = (byte)(value >> 24);
+                    _buffer[5] = (byte)(value >> 16);
+                    _buffer[6] = (byte)(value >> 8);
+                    _buffer[7] = (byte)value;
+                }
+                else
+                {
+                    _buffer[0] = (byte)value;
+                    _buffer[1] = (byte)(value >> 8);
+                    _buffer[2] = (byte)(value >> 16);
+                    _buffer[3] = (byte)(value >> 24);
+                    _buffer[4] = (byte)(value >> 32);
+                    _buffer[5] = (byte)(value >> 40);
+                    _buffer[6] = (byte)(value >> 48);
+                    _buffer[7] = (byte)(value >> 56);
                 }
             }
-            stream.Write(buffer, 0, 8);
+            _stream.Write(_buffer, 0, 8);
         }
 
-
-        public void Write(float value) {
-            ulong tmpValue = *(uint*)&value;
-            unchecked {
-                if (swapNeeded) {
-                    buffer[0] = (byte)(tmpValue >> 24);
-                    buffer[1] = (byte)(tmpValue >> 16);
-                    buffer[2] = (byte)(tmpValue >> 8);
-                    buffer[3] = (byte)tmpValue;
-                } else {
-                    buffer[0] = (byte)tmpValue;
-                    buffer[1] = (byte)(tmpValue >> 8);
-                    buffer[2] = (byte)(tmpValue >> 16);
-                    buffer[3] = (byte)(tmpValue >> 24);
+        /// <inheritdoc />
+        public override void Write(float value)
+        {
+            unsafe
+            {
+                ulong tmpValue = *(uint*)&value;
+                unchecked {
+                    if (_swapNeeded) {
+                        _buffer[0] = (byte)(tmpValue >> 24);
+                        _buffer[1] = (byte)(tmpValue >> 16);
+                        _buffer[2] = (byte)(tmpValue >> 8);
+                        _buffer[3] = (byte)tmpValue;
+                    } else {
+                        _buffer[0] = (byte)tmpValue;
+                        _buffer[1] = (byte)(tmpValue >> 8);
+                        _buffer[2] = (byte)(tmpValue >> 16);
+                        _buffer[3] = (byte)(tmpValue >> 24);
+                    }
                 }
             }
-            stream.Write(buffer, 0, 4);
+            _stream.Write(_buffer, 0, 4);
         }
 
-
-        public void Write(double value) {
-            ulong tmpValue = *(ulong*)&value;
-            unchecked {
-                if (swapNeeded) {
-                    buffer[0] = (byte)(tmpValue >> 56);
-                    buffer[1] = (byte)(tmpValue >> 48);
-                    buffer[2] = (byte)(tmpValue >> 40);
-                    buffer[3] = (byte)(tmpValue >> 32);
-                    buffer[4] = (byte)(tmpValue >> 24);
-                    buffer[5] = (byte)(tmpValue >> 16);
-                    buffer[6] = (byte)(tmpValue >> 8);
-                    buffer[7] = (byte)tmpValue;
-                } else {
-                    buffer[0] = (byte)tmpValue;
-                    buffer[1] = (byte)(tmpValue >> 8);
-                    buffer[2] = (byte)(tmpValue >> 16);
-                    buffer[3] = (byte)(tmpValue >> 24);
-                    buffer[4] = (byte)(tmpValue >> 32);
-                    buffer[5] = (byte)(tmpValue >> 40);
-                    buffer[6] = (byte)(tmpValue >> 48);
-                    buffer[7] = (byte)(tmpValue >> 56);
+        /// <inheritdoc />
+        public override void Write(double value)
+        {
+            unsafe
+            {
+                ulong tmpValue = *(ulong*)&value;
+                unchecked
+                {
+                    if (_swapNeeded)
+                    {
+                        _buffer[0] = (byte)(tmpValue >> 56);
+                        _buffer[1] = (byte)(tmpValue >> 48);
+                        _buffer[2] = (byte)(tmpValue >> 40);
+                        _buffer[3] = (byte)(tmpValue >> 32);
+                        _buffer[4] = (byte)(tmpValue >> 24);
+                        _buffer[5] = (byte)(tmpValue >> 16);
+                        _buffer[6] = (byte)(tmpValue >> 8);
+                        _buffer[7] = (byte)tmpValue;
+                    }
+                    else
+                    {
+                        _buffer[0] = (byte)tmpValue;
+                        _buffer[1] = (byte)(tmpValue >> 8);
+                        _buffer[2] = (byte)(tmpValue >> 16);
+                        _buffer[3] = (byte)(tmpValue >> 24);
+                        _buffer[4] = (byte)(tmpValue >> 32);
+                        _buffer[5] = (byte)(tmpValue >> 40);
+                        _buffer[6] = (byte)(tmpValue >> 48);
+                        _buffer[7] = (byte)(tmpValue >> 56);
+                    }
                 }
             }
-            stream.Write(buffer, 0, 8);
+
+            _stream.Write(_buffer, 0, 8);
         }
 
+        /// <inheritdoc />
+        public override void Write(string value)
+        {
+            if (value is null)
+                throw new ArgumentNullException(nameof(value));
 
-        // Based on BinaryWriter.Write(String)
-        public void Write([NotNull] string value) {
-            if (value == null) {
-                throw new ArgumentNullException("value");
+            var byteLength = Encoding.GetByteCount(value);
+            Write((short)byteLength);
+
+            if (byteLength <= BufferSize)
+            {
+                // string can be written at once if it fits within the buffer size
+                Encoding.GetBytes(value, 0, value.Length, _buffer, 0);
+                _stream.Write(_buffer, 0, byteLength);
+                return;
             }
 
-            // Write out string length (as number of bytes)
-            int numBytes = Encoding.GetByteCount(value);
-            Write((short)numBytes);
-
-            if (numBytes <= BufferSize) {
-                // If the string fits entirely in the buffer, encode and write it as one
-                Encoding.GetBytes(value, 0, value.Length, buffer, 0);
-                stream.Write(buffer, 0, numBytes);
-            } else {
-                // Aggressively try to not allocate memory in this loop for runtime performance reasons.
-                // Use an Encoder to write out the string correctly (handling surrogates crossing buffer
-                // boundaries properly).  
-                int charStart = 0;
-                int numLeft = value.Length;
-                while (numLeft > 0) {
-                    // Figure out how many chars to process this round.
-                    int charCount = (numLeft > MaxBufferedStringLength) ? MaxBufferedStringLength : numLeft;
+            unsafe
+            {
+                // aggressively try to not allocate memory in this loop for runtime performance reasons.
+                // use an Encoder to write out the string correctly (handling surrogates crossing buffer boundaries properly)  
+                var charStart = 0;
+                var numLeft = value.Length;
+                
+                while (numLeft > 0)
+                {
+                    // figure out how many chars to process this round.
+                    var charCount = (numLeft > MaxBufferedStringLength) ? MaxBufferedStringLength : numLeft;
                     int byteLen;
-                    fixed (char* pChars = value) {
-                        fixed (byte* pBytes = buffer) {
-                            byteLen = encoder.GetBytes(pChars + charStart, charCount, pBytes, BufferSize,
-                                                       charCount == numLeft);
+                    
+                    fixed (char* pChars = value)
+                    {
+                        fixed (byte* pBytes = _buffer)
+                        {
+                            byteLen = _encoder.GetBytes(pChars + charStart, charCount, pBytes, BufferSize, 
+                                                        charCount == numLeft);
                         }
                     }
-                    stream.Write(buffer, 0, byteLen);
+
+                    _stream.Write(_buffer, 0, byteLen);
                     charStart += charCount;
                     numLeft -= charCount;
                 }
             }
         }
 
-
-        public void Write(byte[] data, int offset, int count) {
-            int written = 0;
-            while (written < count) {
-                int toWrite = Math.Min(MaxWriteChunk, count - written);
-                stream.Write(data, offset + written, toWrite);
-                written += toWrite;
+        /// <inheritdoc />
+        public override void Write(byte[] data, int offset, int count)
+        {
+            var bytesWritten = 0;
+            
+            while (bytesWritten < count)
+            {
+                var bytesToWrite = Math.Min(MaxWriteChunk, count - bytesWritten);
+                _stream.Write(data, offset + bytesWritten, bytesToWrite);
+                bytesWritten += bytesToWrite;
             }
         }
     }
